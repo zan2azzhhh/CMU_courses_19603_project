784530beb2e0af7257a684d206cf1231ae5dfb6a
xinyewWSL <wangxinye1996@gmail.com>
2023-04-15 18:18:34 -0400

    csv column name modified

--- dataset/NYCBuildingEnergyUse/removeSpaces.py
+++ dataset/NYCBuildingEnergyUse/removeSpaces.py
@@ -1,4 +1,5 @@
 with open("./nyc_benchmarking_disclosure_data_reported_in_2016.csv") as f:
     s = f.read().splitlines()
 
-    print(s[0].split(","))
+    for i in range(100):
+        print((s[i].count(",")))
